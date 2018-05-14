2b3ddaf04d97fe600f5f7ec2d161a38d64d2f16b
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-13 22:16:47 -0700

    setting up api

--- AngularApp/src/app/http.service.ts
+++ AngularApp/src/app/http.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    this.catchEmAll();
+  }
+  catchEmAll(){
+    const blastoise = this._http.get('https://pokeapi.co/api/v2/pokemon/9/');
+    tempObservable.subscribe(data => console.log('Got our tasks!', data));
+  }
 }
