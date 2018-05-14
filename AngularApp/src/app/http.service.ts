d997c20c73756f715e86caea6521191b15e2ca5d
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-13 22:38:59 -0700

    setting up api

--- AngularApp/src/app/http.service.ts
+++ AngularApp/src/app/http.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface PokemonData {
+  name:  string;
+  stats:  {stat: {name: string, base_stat: number}}[];
+}
+
+interface PokeStatData {
+  stat: { name: string, base_stat: number };
+  pokemon: {pokemon: {name: string}};
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +19,19 @@ export class HttpService {
   constructor(private _http: HttpClient) {
     this.catchEmAll();
   }
-  catchEmAll(){
-    const blastoise = this._http.get('https://pokeapi.co/api/v2/pokemon/9/');
-    tempObservable.subscribe(data => console.log('Got our tasks!', data));
+  catchEmAll()  {
+    const blastoiseObservable = this._http.get('https://pokeapi.co/api/v2/pokemon/9/');
+    blastoiseObservable.subscribe(this.blastoiseStatsData);
+  }
+
+  blastoiseStatsData = (data: PokemonData) => {
+    let stats = `${data.name} has the following stats:`;
+    for (let i = 0; i < data.stats.length; i++) {
+      stats += `${data.stats[i].stat.name} : ${data.stats[i].stat.base_stat}`;
+    }
+  }
+
+  statData = (data: PokeStatData) => {
+
   }
 }
