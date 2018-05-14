e1f5faebebcf51f3521b98965b67346f10a867c1
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-13 22:56:02 -0700

    setting up api

--- AngularApp/src/app/http.service.ts
+++ AngularApp/src/app/http.service.ts
@@ -28,10 +28,19 @@ export class HttpService {
     let stats = `${data.name} has the following stats:`;
     for (let i = 0; i < data.stats.length; i++) {
       stats += `${data.stats[i].stat.name} : ${data.stats[i].stat.base_stat}`;
+      console.log(stats);
+    }
+    for (let i = 0; i < data.stats.length; i++) {
+      const statObservable = this._http.get(`https://pokeapi.co/api/v2/stat/${data.stats[i].stat.name}`);
+      statObservable.subscribe(this.statData);
     }
   }
 
   statData = (data: PokeStatData) => {
-
+  let others = `Here are some pokemon with ${data.stat.name} ${data.stat.base_stat}:`;
+  for (let i = 0; i < data.pokemon.length; i++) {
+    others += `${data.pokemon[i].pokemon.name}`;
+    console.log(others);
+    }
   }
 }
